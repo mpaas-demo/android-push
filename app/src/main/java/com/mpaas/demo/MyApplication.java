a4b8874f49abe4a1505d03fbc7c143a4cda00dc3
重岳 <rocky.lqp@antfin.com>
2020-06-18 13:52:20 +0800

    update baseline

--- app/src/main/java/com/mpaas/demo/MyApplication.java
+++ app/src/main/java/com/mpaas/demo/MyApplication.java
@@ -3,6 +3,7 @@ package com.mpaas.demo;
 import android.app.Application;
 import android.content.Context;
 
+import com.mpaas.demo.push.LauncherActivity;
 import com.mpaas.mps.adapter.api.MPPush;
 
 
@@ -17,5 +18,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        MPPush.setBadgeAutoClearEnabled(this, true);
+        MPPush.setBadgeActivityClassName(this, LauncherActivity.class.getName());
     }
 }
